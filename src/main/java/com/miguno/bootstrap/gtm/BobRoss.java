acfc6308b63aa8547a7848dfb1be893d1d19c548
Svilen Ivanov <svilen.ivanov@gmail.com>
2015-12-25 13:40:19 +0200

    Added ability to detect outdated dependencies, added logback, bumped gradle to 2.10

--- src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
+++ src/main/java/com/miguno/bootstrap/gtm/BobRoss.java
@@ -1,21 +1,25 @@
 package com.miguno.bootstrap.gtm;
 
-import java.io.PrintStream;
-import java.util.Set;
-
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSet.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.PrintStream;
+import java.util.Set;
 
 /**
  * A simple class that implements a few features that we can write unit tests for. It uses Google Guava for the sole
  * reason that we have an external compile-time dependency in this bootstrap project.
- * 
+ * <p/>
  * To make the example code simpler this class does not follow pure OO style. (If it did, we would create a Painting
  * object from painting elements and let our painter paint that painting object, for instance.)
- * 
+ * <p/>
  * "And from all of us here I'd like to wish you happy painting and God bless, my friend."
  */
 public class BobRoss {
+
+    private final static Logger log = LoggerFactory.getLogger(BobRoss.class);
 
     private final Set<String> paintingElements;
     private final PrintStream printStream;
@@ -44,6 +48,7 @@ public class BobRoss {
     }
 
     public static void main(String[] args) {
+        log.info("Test message");
         Set<String> paintingElements = ImmutableSet.of("sky", "lake", "mountain", "happy tree", "little squirrel");
         BobRoss bob = new BobRoss(paintingElements);
         bob.paintPicture();
